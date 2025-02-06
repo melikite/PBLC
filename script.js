b8a84611aa2f80d80a2c8cc7cc6034868dc52b9e
melikite <kerim.t.cakmak@gmail.com>
2025-02-06 17:53:47 +0000

    Add files via upload

--- script.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const button = document.getElementById('trigger-animation');
-  button.addEventListener('click', () => {
-    const pen2 = document.getElementById('pen2');
-    pen2.contentWindow.postMessage('start-animation', '*');
-  });
-});
