43077aa5fd0b90b249cd04cf31ad0b7665541a13
melikite <kerim.t.cakmak@gmail.com>
2025-02-06 23:38:47 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,20 @@
 document.addEventListener("DOMContentLoaded", () => {
   const button = document.getElementById('trigger-animation');
-  button.addEventListener('click', () => {
-    const pen2 = document.getElementById('pen2');
-    pen2.contentWindow.postMessage('start-animation', '*');
-  });
-});
+  const pen2 = document.getElementById('pen2');
 
+  if (button && pen2) {
+    button.addEventListener('click', () => {
+      // Ensure the iframe is fully loaded before sending the message
+      pen2.onload = () => {
+        pen2.contentWindow.postMessage('start-animation', '*');
+      };
+
+      // Send the message immediately if the iframe is already loaded
+      if (pen2.contentWindow) {
+        pen2.contentWindow.postMessage('start-animation', '*');
+      }
+    });
+  } else {
+    console.error('Button or iframe not found in the DOM');
+  }
+});
