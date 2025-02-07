59afa44d13aafaab72a96b5603f3bb20eccb7a68
melikite <kerim.t.cakmak@gmail.com>
2025-02-07 16:02:53 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
 document.addEventListener("DOMContentLoaded", () => {
   const button = document.getElementById('trigger-animation');
-  button.addEventListener('click', () => {
-    const pen2 = document.getElementById('pen2');
-    pen2.contentWindow.postMessage('start-animation', '*');
-  });
-});
+  const pen2 = document.getElementById('pen2');
 
+  if (button && pen2) {
+    button.addEventListener('click', () => {
+      if (pen2.contentWindow) {
+        // Specify the exact origin of the iframe instead of '*' for security reasons
+        pen2.contentWindow.postMessage('start-animation', 'https://expected-origin.com');
+      }
+    });
+  } else {
+    console.error('Button or iframe element not found');
+  }
+});
