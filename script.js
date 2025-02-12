27494fadbbcea382d73f5c95c7adb146bea32faa
melikite <kerim.t.cakmak@gmail.com>
2025-02-12 14:39:13 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
 document.addEventListener("DOMContentLoaded", () => {
   const button = document.getElementById('trigger-animation');
-  button.addEventListener('click', () => {
-    const pen2 = document.getElementById('pen2');
-    pen2.contentWindow.postMessage('start-animation', '*');
-  });
-});
+  const pen2 = document.getElementById('pen2');
 
+  if (button && pen2) {
+    button.addEventListener('click', () => {
+      pen2.contentWindow.postMessage('start-animation', 'https://codepen.io');
+    });
+  } else {
+    console.error('Button or iframe not found. Make sure they exist in the HTML.');
+  }
+});
