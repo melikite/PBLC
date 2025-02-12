9c105afa5d3a6c9922b8031b3014d88acb6b59e5
melikite <kerim.t.cakmak@gmail.com>
2025-02-12 14:36:16 +0000

    Update script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const button = document.getElementById('trigger-animation');
+  button.addEventListener('click', () => {
+    const pen2 = document.getElementById('pen2');
+    pen2.contentWindow.postMessage('start-animation', '*');
+  });
+});
+
